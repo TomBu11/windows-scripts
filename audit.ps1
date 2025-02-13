81674fe954ee440427c61532b6fe1edcd46ee547
Tom <tom@rocksalt.cc>
2025-02-13 13:09:15 +0000

    Updated audit.ps1 to fit excel format

--- audit.ps1
+++ audit.ps1
@@ -9,17 +9,13 @@ if (-not (Test-Path -Path $outputDirectory)) {
 }
 
 $memoryType = @{
-  20 = "DDR"
-  21 = "DDR2"
-  24 = "DDR3"
-  26 = "DDR4"
-  32 = "LPDDR"
-  33 = "LPDDR2"
-  34 = "LPDDR3"
-  35 = "LPDDR4"
+  21 = "DDR 2"
+  24 = "DDR 3"
+  26 = "DDR 4"
 }
 
-# Get system information
+# Get audit information
+$date = Get-Date -Format "yyyy-MM-dd"
 $pcName = $env:COMPUTERNAME
 $manufacturer = (Get-WmiObject -Class Win32_ComputerSystem).Manufacturer
 $model = (Get-WmiObject -Class Win32_ComputerSystem).Model
@@ -27,12 +23,13 @@ $serialNumber = (Get-WmiObject -Class Win32_BIOS).SerialNumber
 $os = (Get-WmiObject -Class Win32_OperatingSystem).Caption
 $processor = (Get-WmiObject -Class Win32_Processor).Name
 $ram = (Get-CimInstance -ClassName Win32_PhysicalMemory | Measure-Object -Property Capacity -Sum).Sum / 1GB
-$ramType = Get-CimInstance -ClassName Win32_PhysicalMemory | Select-Object @{Name="MemoryType"; Expression={$memoryType[$_.MemoryType]}}
+$ramType = 
+Get-CimInstance -ClassName Win32_PhysicalMemory | Select-Object @{Name="MemoryType"; Expression={if ($memoryType[$_.MemoryType]) {$memoryType[$_.MemoryType]} else {"-"}}}
 $diskSize = [math]::Round((Get-WmiObject -Class Win32_LogicalDisk -Filter "DeviceID='C:'").Size / 1GB)
-$diskType = Get-CimInstance -ClassName Win32_DiskDrive | Select-Object Model
+$diskType = Get-PhysicalDisk | Select-Object -ExpandProperty MediaType
 
 # Prepare tab-separated line
-$line = "$pcName`t$manufacturer`t$model`t$serialNumber`t$os`t$processor`t$ram`t$diskSize"
+$line = "`t$Date`tno`tGI`t$pcName`t$manufacturer`t$model`t`t$serialNumber`t$os`t`t`t`t`t`t`t`t`t`t$processor`t$ram`t$ramType`t$diskSize`t$diskType`t`t`t"
 
 # Append to the output file
 $line | Out-File -Append -FilePath $outputFile
