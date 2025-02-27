4b1f237a4d2f107aa20935dadeba471fa9d8d06e
Tom <tom@rocksalt.cc>
2025-02-27 11:17:51 +0000

    Added domain name and local rocksalt account to audit

--- usb-audit.ps1
+++ usb-audit.ps1
@@ -29,14 +29,21 @@ $manufacturer = (Get-WmiObject -Class Win32_ComputerSystem).Manufacturer
 $model = (Get-WmiObject -Class Win32_ComputerSystem).Model
 $serialNumber = (Get-WmiObject -Class Win32_BIOS).SerialNumber
 $os = (Get-WmiObject -Class Win32_OperatingSystem).Caption
+# Check if the local Rocksalt exists
+$rocksaltExists = if (Get-LocalUser -Name "Rocksalt" -ErrorAction SilentlyContinue) {
+  "Yes"
+} else {
+  "No"
+}
+$domainName = (Get-WmiObject -Class Win32_ComputerSystem).Domain
 $processor = (Get-WmiObject -Class Win32_Processor).Name
 $ram = (Get-CimInstance -ClassName Win32_PhysicalMemory | Measure-Object -Property Capacity -Sum).Sum / 1GB
-Get-CimInstance -ClassName Win32_PhysicalMemory | Select-Object @{Name="MemoryType"; Expression={if ($memoryType[$_.MemoryType]) {$memoryType[$_.MemoryType]} else {"-"}}}
+# $ramType = Get-CimInstance -ClassName Win32_PhysicalMemory | Select-Object @{Name="MemoryType"; Expression={if ($memoryType[$_.MemoryType]) {$memoryType[$_.MemoryType]} else {"-"}}}
 $diskSize = [math]::Round((Get-WmiObject -Class Win32_LogicalDisk -Filter "DeviceID='C:'").Size / 1GB)
 $diskType = Get-PhysicalDisk | Select-Object -ExpandProperty MediaType
 
 # Prepare tab-separated line
-$line = "`t$Date`tno`tGI$gi`t$pcName`t$manufacturer`t$model`t`t$serialNumber`t$os`t`t`t`t`t`t`t`t`t$name`t$processor`t$ram`t`t$diskSize`t$diskType`t`t`tYes"
+$line = "`t$Date`tno`tGI$gi`t$pcName`t$manufacturer`t$model`t`t$serialNumber`t$os`t`t`t`t`t$rocksaltExists`t`t$domainName`t`t$name`t$processor`t$ram`t`t$diskSize`t$diskType`t`t`tYes"
 
 # Append to the output file
 $line | Out-File -Append -FilePath $outputFile
