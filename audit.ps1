34dfd49f71c9c411a981b84b22bef0b7ad4f28be
Tom <tom@rocksalt.cc>
2025-04-28 17:21:44 +0100

    WIP

--- audit.ps1
+++ audit.ps1
@@ -1,69 +1,178 @@
-# Define output file path
+#Requires -RunAsAdministrator
+
+<# INITIAL SETUP #>
+
 $outputDirectory = "C:\Rocksalt"
-$outputFile = "$outputDirectory\Audit.txt"
+$outputFile = Join-Path $outputDirectory "Audit.txt"
 
 # Create the directory if it doesn't exist
 if (-not (Test-Path -Path $outputDirectory)) {
     New-Item -ItemType Directory -Path $outputDirectory
     Write-Host "Directory created: $outputDirectory"
+} else {
+    Write-Host "Directory already exists: $outputDirectory"
 }
 
-$memoryType = @{
-  21 = "DDR 2"
-  24 = "DDR 3"
-  26 = "DDR 4"
+# Run various 'Get' functions and save to local variables
+# (e.g. so that we only have to call Get-ComputerInfo once - it is a very slow function!)
+$ComputerInfo = Get-ComputerInfo
+Write-Output 'Got computer info'
+$RamInfo = Get-WmiObject -Class Win32_PhysicalMemory
+Write-Output 'Got RAM'
+$Users = Get-WmiObject -Class Win32_UserAccount
+Write-Output 'Got users'
+$AdminGroup = Get-WmiObject -Class Win32_Group -Filter "Name='Administrators'"
+Write-Output 'Got admin group'
+$Admins = Get-WmiObject -Query "ASSOCIATORS OF {$AdminGroup} where Role = GroupComponent"
+Write-Output 'Got admins'
+$TeamViewerInfo = Get-ItemProperty -Path HKLM:\Software\Teamviewer
+Write-Output 'Got TeamViewer'
+$KeyProtectors = try { (Get-BitLockerVolume).KeyProtector } catch { null } # REQUIRES ADMIN PERMISSIONS
+Write-Output 'Got BitLocker'
+$PhysicalDisks = Get-PhysicalDisk
+Write-Output 'Got disks'
+$InstalledSoftware = Get-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*, HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*
+Write-Output 'Got software'
+
+
+<# HELPER FUNCTIONS #>
+
+# (from https://www.dmtf.org/sites/default/files/standards/documents/DSP0134_3.4.0a.pdf)
+Function Convert-RamMemoryType([Parameter(Mandatory=$true)]$MemoryTypeDecimal){
+    switch ($MemoryTypeDecimal){
+      00 {'Unknown'}
+      01 {'Other'}
+      02 {'DRAM'}
+      03 {'Synchronous DRAM'}
+      04 {'Cache DRAM'}
+      05 {'EDO'}
+      06 {'EDRAM'}
+      07 {'VRAM'}
+      08 {'SRAM'}
+      09 {'RAM'}
+      10 {'ROM'}
+      11 {'FLASH'}
+      12 {'EEPROM'}
+      13 {'FEPROM'}
+      14 {'EPROM'}
+      15 {'CDRAM'}
+      16 {'3DRAM'}
+      17 {'SDRAM'}
+      18 {'SGRAM'}
+      19 {'RDRAM'}
+      20 {'DDR'}
+      21 {'DDR2'}
+      22 {'DDR FB-DIMM'}
+      24 {'DDR3'}
+      25 {'FBD2'}
+      26 {'DDR4'}
+      27 {'LPDDR'}
+      28 {'LPDDR2'}
+      29 {'LPDDR3'}
+      30 {'LPDDR4'}
+      31 {'Logical non-volatile device'}
+      32 {'HBM'}
+      33 {'HBM2'}
+      34 {'DDR5'}
+      35 {'LPDDR5'}
+      Default {'Unknown'}
+    }
 }
 
-# Set Brute Force rules
+function Read-YesNo($prompt) {
+  return if ((Read-Host "$prompt (y/N)") -match '^y$') { "Yes" } else { "No" }
+}
+
+<# BRUTE FORCE PROTECTION #>
+
 Write-Host "Running brute force commands"
 net accounts /lockoutthreshold:10
 net accounts /lockoutwindow:5
 net accounts /lockoutduration:30
 
-# Get audit information
-$auditer = Read-Host "RS (initials)"
-$date = Get-Date -Format "yyyy-MM-dd"
-$done = "Part"
-$users = Read-Host "Users"
-$gi = Read-Host "GI (numbers)"
-$pcName = $env:COMPUTERNAME
-$manufacturer = (Get-WmiObject -Class Win32_ComputerSystem).Manufacturer
-$model = (Get-WmiObject -Class Win32_ComputerSystem).Model
-$type = if ((Get-WmiObject -Class Win32_ComputerSystem).PCSystemType -eq 2) { "Laptop" } else { "Desktop" }
-$serialNumber = (Get-WmiObject -Class Win32_BIOS).SerialNumber
-$os = (Get-WmiObject -Class Win32_OperatingSystem).Caption
-$win11Comp = if ($os -match "11") { "Yes" } else { "No" }
-$updates = if ((Read-Host "Updates (y/N)") -eq "y") { "Yes" } else { "No" }
-$drivers = if ((Read-Host "Drivers (y/N)") -eq "y") { "Yes" } else { "No" }
-$antiVirus = if ((Read-Host "Antivirus (y/N)") -eq "y") { "Yes" } else { "No" }
-# Check if the local Rocksalt exists
-$rocksaltExists = if (Get-LocalUser -Name "Rocksalt" -ErrorAction SilentlyContinue) {
-  "Yes"
-  Write-Host "Rocksalt user exits"
-} else {
-  "No"
-  Write-Host "Rocksalt user does not exist please create one" -ForegroundColor Red
-}
-$clientAdmin = Read-Host "Client Admin"
-$domainName = (Get-WmiObject -Class Win32_ComputerSystem).Domain
-$userName = Read-Host "Username (Account they use)"
-$processor = (Get-WmiObject -Class Win32_Processor).Name
-$ram = (Get-CimInstance -ClassName Win32_PhysicalMemory | Measure-Object -Property Capacity -Sum).Sum / 1GB
+
+<# AUDIT INFORMATION #>
+
+$clientAdmin = $Admins | Where-Object { $_.LocalAccount -eq $true } | Select-Object -ExpandProperty Name
 # $ramType = Get-CimInstance -ClassName Win32_PhysicalMemory | Select-Object @{Name="MemoryType"; Expression={if ($memoryType[$_.MemoryType]) {$memoryType[$_.MemoryType]} else {"-"}}}
-$diskSize = [math]::Round((Get-WmiObject -Class Win32_LogicalDisk -Filter "DeviceID='C:'").Size / 1GB)
-$diskType = Get-PhysicalDisk | Select-Object -ExpandProperty MediaType
-$bitlocker = if ($bitlockerStatus -eq 1) { "Yes" } else { "No" }
-$teamviewer = Read-Host "Teamviewer ID"
-$bruteForce = "Yes"
-$notes = Read-Host "Notes"
 
-# Prepare tab-separated line
-$line = "$auditer`t$date`t$done`t$users`tGI$gi`t$pcName`t$manufacturer`t$model`t$type`t$serialNumber`t$os`t$win11Comp`t$updates`t$drivers`t$antiVirus`t$rocksaltExists`t$clientAdmin`t$userName`t$domainName`t$processor`t$ram`t$ramType`t$diskSize`t$diskType`t`t`t$bitlocker`t$teamviewer`t$bruteForce`t$notes"
+$auditer         = Read-Host "RS (initials)"
+$date            = Get-Date -Format "yyyy-MM-dd"
+$done            = "Part"
+$users           = Read-Host "Users"
+$gi              = Read-Host "GI (numbers)"
+$pcName          = $env:COMPUTERNAME
+$manufacturer    = $ComputerInfo.CsManufacturer
+$model           = $ComputerInfo.CsModel
+$type            = if ($ComputerInfo.CsPCSystemType -eq 2) { "Laptop" } else { "Desktop" }
+$serialNumber    = $ComputerInfo.BiosSeralNumber
+$os              = $ComputerInfo.OSName
+$win11Comp       = if ($os -match "11") { "Yes" } else { "No" }
+$updates         = Read-YesNo "Updates (y/N)"
+$drivers         = Read-YesNo "Drivers (y/N)"
+$antiVirus       = Read-YesNo "Antivirus (y/N)"
+$rocksaltExists  = if (Get-LocalUser -Name "Rocksalt" -ErrorAction SilentlyContinue) { "Yes" } else { "No" }
+$clientAdmin     = $Admins | Where-Object { $_.LocalAccount -eq $true } | Select-Object -ExpandProperty Name
+$userName        = Read-Host "Username (Account they use)"
+$domainName      = $ComputerInfo.CsDomain
+$processor       = $ComputerInfo.CsProcessors
+$ram             = [math]::Round($ComputerInfo.CsPhysicallyInstalledMemory / 1GB)
+$ramType         = Convert-RamMemoryType -MemoryTypeDecimal ($RamInfo[0].SMBIOSMemoryType)
+$diskSize        = [math]::Round((Get-WmiObject -Class Win32_LogicalDisk -Filter "DeviceID='C:'").Size / 1GB)
+$diskType        = "$($PhysicalDisks.MediaType) $($PhysicalDisks.BusType)"
+$bitlocker       = if ($bitlockerStatus -eq 1) { "Yes" } else { "No" }
+$teamViewer      = $TeamViewerInfo.ClientID
+$bruteForce      = "Yes"
+$notes           = Read-Host "Notes"
 
-# Append to the output file
+
+
+$lineTable = [PSCustomObject]@{
+  Auditer         = $auditer
+  Date            = $date
+  Done            = $done
+  Users           = $users
+  GI              = "GI$gi"
+  PCName          = $pcName
+  Manufacturer    = $manufacturer
+  Model           = $model
+  Type            = $type
+  SerialNumber    = $serialNumber
+  OS              = $os
+  Win11Compatible = $win11Comp
+  Updates         = $updates
+  Drivers         = $drivers
+  AntiVirus       = $antiVirus
+  RocksaltExists  = $rocksaltExists
+  ClientAdmin     = $clientAdmin
+  UserName        = $userName
+  DomainName      = $domainName
+  Processor       = $processor
+  RAM             = $ram
+  RAMType         = $ramType
+  DiskSize        = $diskSize
+  DiskType        = $diskType
+  Empty1          = ""
+  Empty2          = ""
+  Bitlocker       = $bitlocker
+  TeamViewer      = $teamviewer
+  BruteForce      = $bruteForce
+  Notes           = $notes
+}
+
+<# OUTPUT #>
+
+$line = ($lineTable.PSObject.Properties | ForEach-Object { $_.Value }) -join "`t"
+
+Write-Host "`n=== Tab-separated Line ==="
+Write-Host $line
+
+Write-Host "`n=== Vertical Table ==="
+$lineTable | Format-List
+
+
+<# SAVE OUTPUT #>
+
 $line | Out-File -Append -FilePath $outputFile
 
-Write-Host "System information has been appended to $outputFile"
-
-# Wait for user input before exiting
-Read-Host -Prompt "Press Enter to exit"
+Read-Host -Prompt "System information has been appended to $outputFile (Enter to exit)"
